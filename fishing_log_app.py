49e1a644abd386fb004188e2d320c8ceac918cfc
pamogoro <enunpis@gmail.com>
2025-12-11 00:33:24 +0000

    test

--- fishing_log_app.py
+++ fishing_log_app.py
@@ -400,6 +400,7 @@ with tab1:
     )
 
     st.image(tide_img_url, use_column_width=True)
+    st.image("https://api.tide736.net/tide_image.php?pc=28&hc=9&yr=2025&mn=12&dy=11&rg=day&w=768&h=512&lc=blue&gcs=cyan&gcf=blue&ld=on&ttd=on&tsmd=on")
     st.caption("※データ元：tide736.net（日本沿岸736港の潮汐表）")
     st.divider()
 
